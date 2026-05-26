4a600d03f369c2ca6844f12303888034d960e8f4
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-05-26 18:05:02 +0900

    feat: task M added

---
+++ src/train.py
@@ -0,0 +1,23 @@
+# Task M
+result = []
+
+
+def getSquareNumbers(n):
+    for i in n:
+        result.append({"number": i, "square": i * i})
+    return result
+
+
+print(getSquareNumbers([5, 7, 10]))
+
+
+# Task L
+# def reverse_sentence(sentence):
+#     words = sentence.split(" ")
+
+#     reversed_words = [word[::-1] for word in words]
+
+#     return " ".join(reversed_words)
+
+
+# print(reverse_sentence("I like coding!"))
