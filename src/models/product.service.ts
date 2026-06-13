814aaf7f75eac0e93f718e090cff86773fc704ba
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-06-13 17:45:00 +0900

    fix: getAllProducts api and passing data into ejs

--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -14,6 +14,13 @@ class ProductService {
   /** SPA */
 
   /** SSR */
+  public async getAllProducts(): Promise<Product[]> {
+
+    const result = await this.productModel.find().exec();
+    if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND)
+        
+    return result;    
+  }
 
   public async createNewProduct(input: ProductInput): Promise<Product> {
     try {
@@ -31,7 +38,6 @@ class ProductService {
     const result = await this.productModel.findOneAndUpdate({ _id: id}, input, {new: true}).exec();
     if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED)
     
-
     return result;    
   }
 }
--- src/train.py
+++ src/train.py
@@ -1,12 +1,39 @@
+# Task T
+def mergeSortedArrays(a, b):
+    result = []
+    i = 0
+    j = 0
+
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            result.append(a[i])
+            i += 1
+        else:
+            result.append(b[j])
+            j += 1
+
+    while i < len(a):
+        result.append(a[i])
+        i += 1
+
+    while j < len(b):
+        result.append(b[j])
+        j += 1
+
+    return result
+
+
+print(mergeSortedArrays([0, 3, 4], [4, 6]))
+
 # Task s
-def missingNumber(nums):
-    n = len(nums)
-    expected = n * (n + 1) // 2
-    actual = sum(nums)
-    return expected - actual
+# def missingNumber(nums):
+#     n = len(nums)
+#     expected = n * (n + 1) // 2
+#     actual = sum(nums)
+#     return expected - actual
 
 
-print(missingNumber([3, 0, 1]))
+# print(missingNumber([3, 0, 1]))
 
 # Task R
 # def calculate(text):
