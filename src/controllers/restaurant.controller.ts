d164d685f0a0b7be9401f63b078fe23666d096d8
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-06-18 16:18:27 +0900

    feat: devvelop uptadeChosenProduct business logic

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -111,12 +111,16 @@ restaurantController.getUsers = async (req: Request, res: Response) => {
     }
 };
 
-restaurantController.updateChosenUser = function (req: Request, res: Response) {
+restaurantController.updateChosenUser = async (req: Request, res: Response) => {
     try {
-        res.render('updateChosenUser');
+        const memberService = new MemberService();
+        const result = await memberService.updateChosenUser(req.body);
+
+        res.status(HttpCode.OK).json({data: result});
     } catch (err) {
         console.log('Error in updateChosenUser:', err);
-        res.redirect('/admin');
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
     }
 };
 
--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -35,6 +35,17 @@ export interface LogInput {
   memberPassword: string;
 }
 
+export interface MemberUpdateInput {
+  _id: ObjectId;
+  memberStatus?: MemberStatus;
+  memberNick?: string;
+  memberPhone?: string;
+  memberPassword?: string;
+  memberAddress?: string;
+  memberDesc?: string;
+  memberImage?: string;
+}
+
 export interface AdminRequest extends Request {
     member: Member;
     session: Session & { member: Member };
