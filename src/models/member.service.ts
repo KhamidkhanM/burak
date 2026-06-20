9222a4311393647014f940cee07c57f569363ed7
KhamidkhanM <HoshimalievHamidxonn@gmail.com>
2026-06-20 16:06:22 +0900

    fix: small login bug fix

--- src/models/member.service.ts
+++ src/models/member.service.ts
@@ -47,7 +47,7 @@ class MemberService {
       throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
     }
 
-    return await this.memberModel.findOne(member._id).lean().exec();
+    return await this.memberModel.findOne({ _id: member._id }).lean().exec();
   }
 
   /** SSR */
